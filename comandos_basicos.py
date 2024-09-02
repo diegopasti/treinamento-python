c832c9a312bc0d814f94d9d03d0cf480dafd2c96
diego <diego.pasti@tegrus.com.br>
2024-09-01 21:58:27 -0300

    Inclusão dos comandos basicos

--- comandos_basicos.py
+++ comandos_basicos.py
@@ -1,1 +1,36 @@
-print("Bem-vindo")
+# Funções basicas
+nome = input("DIGITE SEU NOME:")
+print("SEU NOME É:", nome)
+
+# Operações Matematica
+soma = 10 + 20
+subtracao = 10 - 20
+multiplicao = 10 * 20
+divisão = 10 / 20
+potencia = 2 ** 3
+
+# Operadores Lógicos
+# maior/menor: ><
+# Maior igual: >=
+# E: and
+# OU: or
+# CONTEM: in
+
+
+# Condicionais
+if "PASTI" in nome:
+    print("")
+else:
+    print("ESSE CARA NÂO É ITALIANO")
+
+# Repetições
+
+soma = 0
+while soma < 10:
+    valor = int(input("DIGITE UM NUMERO DE NOVO:"))
+    soma = soma + valor
+    print("SOMA:", soma)
+
+print("ENFIM, SAI DESSE LOOP")
+
+
