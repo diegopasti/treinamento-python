e78af931f0846ac7ebb6ea75bc3f5977bbd8490a
diego <diego.pasti@tegrus.com.br>
2024-11-24 13:13:22 -0300

    Criando exemplo pra exercicios

---
+++ exercicios/basico/atribuicao_variaveis.py
@@ -0,0 +1,25 @@
+"""
+Atribuição de variáveis
+
+Permite que valores sejam armazenados em memória e possam ser
+acessados no escopo no qual está definido.
+
+     x     =     10
+     |     |      |-> Valor a ser armazenado
+     |     |
+     |     |-> Comando de atribuição
+     |
+     |-> Nome da variavel
+
+
+Nesse caso um espaço de memória é reservado na memória e recebe um
+identificador interno para guardar a posição e um nome.
+
+Variáveis que não são mais utilizados é descartado automaticamente.
+"""
+
+# Atribuindo uma variavel contendo um valor numérico inteiro
+idade = 10
+
+# Atribuindo uma variável contendo um valor lógico
+resultado = True
